12688ee2613e34d506887f94d0258f452787fefc
Gustavo Nunes <gustavo_anunes@hotmail.com>
2021-07-01 11:15:33 -0300

    Melhorando a implementação do método tabuleiro

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -9,19 +9,37 @@ namespace xadrez_console
         {
             for (int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.Colunas; j++)
-                {
+                {                    
                     if (tab.peca(i, j) == null)
                     {
                         Console.Write("- ");
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
+                        ImprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
-            }            
+            }
+            Console.WriteLine("  a b c d e f g j");
+        }
+
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.WriteLine(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
         }
     }
 }
